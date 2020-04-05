04ee793a700c1d5032dd31b78295212efb0e21c5
Sebastian Chesa <sebastian.chesa@berg-software.com>
2020-04-06 01:33:15 +0300

    add get_urls

--- imgflip_scraper/spiders/memes_spider.py
+++ imgflip_scraper/spiders/memes_spider.py
@@ -1,16 +1,36 @@
 import json
 import scrapy
+import csv
+import string
+import os
 
 
 class MemesSpider(scrapy.Spider):
     name = "memes"
 
+    def get_urls(self):
+        csv_filename = 'popular_100_memes.csv'
+        with open(csv_filename) as csv_file:
+            reader = csv.reader(csv_file, delimiter=',')
+            line = 0
+            for row in reader:
+                line += 1
+                if line == 1:
+                    self.log('HEADERS\n%s %s %s' % (row[0], row[1], row[2]))
+                else:
+                    self.log('%s %s %s' % (row[0], row[1], row[2]))
+                    url = 'https://imgflip.com/memetemplate/'
+                    # delete punctuations
+                    table = str.maketrans(dict.fromkeys(string.punctuation))
+                    title = row[1].translate(table)
+                    yield 'https://imgflip.com/memetemplate/'+"-".join(title.split())  # join words with -
+
     def start_requests(self):
-        # TODO get top 100 memes from https://api.imgflip.com/popular_meme_ids
-        urls = [
-            'https://imgflip.com/memetemplate/Distracted-Boyfriend',
-        ]
-        for url in urls:
+        # urls = [
+        #     'https://imgflip.com/memetemplate/Distracted-Boyfriend',
+        # ]
+        for url in self.get_urls():
+            self.log('scraping URL:%s' % url)
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
@@ -20,10 +40,12 @@ class MemesSpider(scrapy.Spider):
         """
 
         # TODO get list of bounding boxes from /memegenerator/
-        #
+
         page = response.url.split("/")[-1]
         filename = '%s.json' % page
-        with open(filename, 'w+') as file:
+        save_path = 'D:/Other Projects/memes/scrappy/imgflip_scraper/dataset/'
+        complete_name = os.path.join(save_path, filename)
+        with open(complete_name, 'w+') as file:
             meme = {'template': response.css('#mtm-img::attr(src)').get()}
             self.log('meme object %s' % meme)
             json.dump(meme, file)
